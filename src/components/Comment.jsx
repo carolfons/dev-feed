8a292381b5e8719ce826f729f3ed7bc31f94f36f
CarolFons <carolsantosfonseca@outlook.com>
2024-10-20 22:10:02 -0300

    :art: Layout Update | Styling comment section done

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -12,16 +12,15 @@ export function Comment() {
                         <strong>Diego Fernandes</strong>
                         <time title="11 de Maio às 08:13h" dateTime="2023-05-11 08:13:30">Cerca de 2h atrás</time>
                     </div>
-                    <button title='Deletar comentário'><Trash size={20}/></button>
+                    <button title='Deletar comentário'><Trash size={24}/></button>
                 </header>
-                <p>Muito Bom Carol! Parabéns</p>
-
+                <p>Muito Bom Carol! Parabéns 🔥🚀</p>
             </div>
             <footer>  
-                <button><ThumbsUp/></button>
-                Like <span>20</span>
+                <button><ThumbsUp/>Like <span>20</span></button>
             </footer>
         </div>
     </div>
   )
 }
+
